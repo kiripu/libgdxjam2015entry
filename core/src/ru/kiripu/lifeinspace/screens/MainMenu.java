7c6b16a74762d9956bcfae8d0fdb99e597de4296
isdiev@gmail.com <isdiev@gmail.com>
2016-01-06 00:57:59 +0300

    Menu sprite moved into sprite sheet

--- core/src/ru/kiripu/lifeinspace/screens/MainMenu.java
+++ core/src/ru/kiripu/lifeinspace/screens/MainMenu.java
@@ -2,9 +2,8 @@ package ru.kiripu.lifeinspace.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -32,48 +31,51 @@ public class MainMenu implements Screen
     private AnimatedSprite heroAnimSprite;
 
     @Override
-    public void show() {
+    public void show()
+    {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         table = new Table();
         table.setFillParent(true);
         table.top();
-        table.setDebug(true);
+//        table.setDebug(true);
 
-        title = new Image(new Texture("mainMenu/title.png"));
+        TextureAtlas atlas  = Main.assetsController.getTextureAtlas();
+
+        title = new Image(atlas.createSprite("mainMenu/title"));
 
         Button.ButtonStyle style = new Button.ButtonStyle();
-        style.up = new SpriteDrawable(new Sprite(new Texture("mainMenu/onePlayerButton_up.png")));
-        style.down = new SpriteDrawable(new Sprite(new Texture("mainMenu/onePlayerButton_down.png")));
-        style.over = new SpriteDrawable(new Sprite(new Texture("mainMenu/onePlayerButton_hover.png")));
+        style.up = new SpriteDrawable(atlas.createSprite("mainMenu/onePlayerButton_up"));
+        style.down = new SpriteDrawable(atlas.createSprite("mainMenu/onePlayerButton_down"));
+        style.over = new SpriteDrawable(atlas.createSprite("mainMenu/onePlayerButton_hover"));
         onePlayerButton = new Button(style);
 
         style = new Button.ButtonStyle();
-        style.up = new SpriteDrawable(new Sprite(new Texture("mainMenu/twoPlayerButton_up.png")));
-        style.down = new SpriteDrawable(new Sprite(new Texture("mainMenu/twoPlayerButton_down.png")));
-        style.over = new SpriteDrawable(new Sprite(new Texture("mainMenu/twoPlayerButton_hover.png")));
+        style.up = new SpriteDrawable(atlas.createSprite("mainMenu/twoPlayerButton_up"));
+        style.down = new SpriteDrawable(atlas.createSprite("mainMenu/twoPlayerButton_down"));
+        style.over = new SpriteDrawable(atlas.createSprite("mainMenu/twoPlayerButton_hover"));
         twoPlayersButton = new Button(style);
 
         style = new Button.ButtonStyle();
-        style.up = new SpriteDrawable(new Sprite(new Texture("mainMenu/topSurvivors_up.png")));
-        style.down = new SpriteDrawable(new Sprite(new Texture("mainMenu/topSurvivors_down.png")));
-        style.over = new SpriteDrawable(new Sprite(new Texture("mainMenu/topSurvivors_hover.png")));
+        style.up = new SpriteDrawable(atlas.createSprite("mainMenu/topSurvivors_up"));
+        style.down = new SpriteDrawable(atlas.createSprite("mainMenu/topSurvivors_down"));
+        style.over = new SpriteDrawable(atlas.createSprite("mainMenu/topSurvivors_hover"));
         topSurvivors = new Button(style);
 
-        heroAnim = new Animation(0.03f, Main.assetsController.getTextureAtlas().findRegions("hero/idle"));
+        heroAnim = new Animation(0.03f, atlas.findRegions("hero/idle"));
         heroAnim.setPlayMode(Animation.PlayMode.LOOP);
         heroAnimSprite = new AnimatedSprite(heroAnim);
         heroAnimSprite.setAutoUpdate(true);
         heroAnimSprite.play();
         heroImage = new Image(heroAnimSprite);
         heroImage.setOrigin(Align.center);
-        heroImage.addAction(Actions.forever(Actions.rotateBy(20f, 1f)));
+        heroImage.addAction(Actions.forever(Actions.rotateBy(-30f, 1f)));
 
-        table.add(title).padTop(150).row();
-        table.add(heroImage).spaceTop(100).row();
-        table.add(onePlayerButton).spaceTop(50).row();
-        table.add(twoPlayersButton).spaceTop(50).row();
-        table.add(topSurvivors).spaceTop(50).row();
+        table.add(title).padTop(160).row();
+        table.add(heroImage).padTop(60).row();
+        table.add(onePlayerButton).padTop(50).row();
+        table.add(twoPlayersButton).padTop(20).row();
+        table.add(topSurvivors).padTop(20).row();
         stage.addActor(table);
     }
 
