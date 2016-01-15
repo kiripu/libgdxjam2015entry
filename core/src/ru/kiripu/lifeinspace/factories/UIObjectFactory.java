cfb8bd428d3621d85ff8aca50db2f261b9348ccc
isdiev@gmail.com <isdiev@gmail.com>
2016-01-16 02:11:15 +0300

    Added ui into game screen

--- core/src/ru/kiripu/lifeinspace/factories/UIObjectFactory.java
+++ core/src/ru/kiripu/lifeinspace/factories/UIObjectFactory.java
@@ -2,6 +2,8 @@ package ru.kiripu.lifeinspace.factories;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import ru.kiripu.lifeinspace.Main;
 
@@ -14,18 +16,38 @@ public class UIObjectFactory
     private static final String BUTTON_STATE_UP = "_up";
     private static final String BUTTON_STATE_DOWN = "_down";
     private static final String BUTTON_STATE_HOVER = "_hover";
+    private static final String PROGRESS_BAR_BACKGROUND = "_back";
+    private static final String PROGRESS_BAR_FILL = "_fill";
     private static TextureAtlas textureAtlas;
 
 
-    public static Button createButton(String buttonName)
+    public static Button createButton(String name)
     {
         Button.ButtonStyle buttonStyle = new Button.ButtonStyle();
-        buttonStyle.up = new SpriteDrawable(getAtlas().createSprite(UI_PATH + buttonName + BUTTON_STATE_UP));
-        buttonStyle.down = new SpriteDrawable(getAtlas().createSprite(UI_PATH + buttonName + BUTTON_STATE_DOWN));
-        buttonStyle.over = new SpriteDrawable(getAtlas().createSprite(UI_PATH + buttonName + BUTTON_STATE_HOVER));
+        buttonStyle.up = new SpriteDrawable(getAtlas().createSprite(UI_PATH + name + BUTTON_STATE_UP));
+        buttonStyle.down = new SpriteDrawable(getAtlas().createSprite(UI_PATH + name + BUTTON_STATE_DOWN));
+        buttonStyle.over = new SpriteDrawable(getAtlas().createSprite(UI_PATH + name + BUTTON_STATE_HOVER));
         Button button = new Button(buttonStyle);
-        button.setName(buttonName);
+        button.setName(name);
         return button;
+    }
+
+    public static Image createImage(String name)
+    {
+        Image image = new Image(getAtlas().createSprite(UI_PATH + name));
+        image.setName(name);
+        return image;
+    }
+
+    public static ProgressBar createProgressBar(String name)
+    {
+        ProgressBar.ProgressBarStyle progressBarStyle = new ProgressBar.ProgressBarStyle();
+        progressBarStyle.background = new SpriteDrawable(getAtlas().createSprite(UI_PATH + name + PROGRESS_BAR_BACKGROUND));
+        progressBarStyle.knobBefore = new SpriteDrawable(getAtlas().createSprite(UI_PATH + name + PROGRESS_BAR_FILL));
+        ProgressBar progressBar = new ProgressBar(0, 100, 0.5f, false, progressBarStyle);
+        progressBar.setValue(0);
+        progressBar.setName(name);
+        return progressBar;
     }
 
     private static TextureAtlas getAtlas()
--- core/src/ru/kiripu/lifeinspace/screens/GameWorldScreen.java
+++ core/src/ru/kiripu/lifeinspace/screens/GameWorldScreen.java
@@ -1,6 +1,14 @@
 package ru.kiripu.lifeinspace.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import ru.kiripu.lifeinspace.factories.UIObjectFactory;
 import ru.kiripu.lifeinspace.world.GameWorld;
 
 /**
@@ -9,11 +17,34 @@ import ru.kiripu.lifeinspace.world.GameWorld;
 public class GameWorldScreen implements Screen {
 
     private final GameWorld world;
+    private final Stage stage;
+    private final Table table;
+    private final Image progressBarLabel;
+    private final ProgressBar progressBar;
+    private final Button soundButton;
 
     public GameWorldScreen()
     {
         world = new GameWorld();
         world.init();
+
+
+        progressBarLabel = UIObjectFactory.createImage("oxygenBar_label");
+        progressBar = UIObjectFactory.createProgressBar("oxygenBar");
+        soundButton = UIObjectFactory.createButton("soundButton");
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        table = new Table();
+
+        table.setFillParent(true);
+        table.bottom();
+        table.add(progressBarLabel).padBottom(20).padLeft(150).bottom();
+        table.add(progressBar).padBottom(20).padLeft(5).bottom();
+        table.add(soundButton).expand().bottom().right().pad(10);
+        stage.addActor(table);
+
+        progressBarLabel.addAction(Actions.forever(Actions.sequence(Actions.alpha(0.3f, 0.75f), Actions.alpha(1, 0.75f))));
     }
 
     @Override
@@ -24,7 +55,10 @@ public class GameWorldScreen implements Screen {
     @Override
     public void render(float delta)
     {
+        progressBar.setSize(380, 7);
         world.update(delta);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
