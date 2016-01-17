5067b8e9d75483a6deb65b1dba40fda5ff80de12
isdiev@gmail.com <isdiev@gmail.com>
2016-01-18 01:28:38 +0300

    Added all other platforms files

---
+++ android/src/ru/kiripu/lifeinspace/android/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package ru.kiripu.lifeinspace.android;
+
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.kiripu.lifeinspace.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
